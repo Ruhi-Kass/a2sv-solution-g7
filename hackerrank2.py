485a81275d4406f1d458868e083f6d39e2f93c31
Ruhama Kassahun <140911522+Ruhi-Kass@users.noreply.github.com>
2026-02-25 15:49:41 +0300

    Add insertion sort function to hackerrank2.py
    Implement insertion sort and print intermediate steps.

---
+++ hackerrank2.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def insertionSort1(n, arr):
+   
+    key = arr[n-1]
+    i = n - 2
+    
+    
+    while i >= 0 and arr[i] > key:
+        arr[i + 1] = arr[i]
+        
+        print(' '.join(map(str, arr)))
+    
+        i -= 1
+      
+    arr[i + 1] = key
+    
+    print(' '.join(map(str, arr))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    insertionSort1(n, arr)
